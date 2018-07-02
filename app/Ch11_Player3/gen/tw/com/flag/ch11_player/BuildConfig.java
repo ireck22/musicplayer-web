9ec67c3004d2f0080d9a7e754a4a3b0c483dca44
ireck22 <dragon9029@gmail.com>
2018-07-02 20:03:30 +0800

    rm app

--- app/Ch11_Player3/gen/tw/com/flag/ch11_player/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package tw.com.flag.ch11_player;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- app/資料庫/qqq/app/src/main/res/timepage.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by 助理001 on 2016/7/19.
- */
-public class timepage {
-}
